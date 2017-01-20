e0c73cdbdd678066612e7834520866267d42217f
Caitlin Hines and Katy Daviscourt <katy@>
2017-01-19 16:10:09 -0800

    attempt js for making the three inputs arrays but it doesnt work

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+
+
+var pushIntoArrays = function(a, b, c){
+  var favoriteArray = [];
+
+  favoriteArray.push(a);
+  favoriteArray.push(b);
+  favoriteArray.push(c);
+  console.log(favoriteArray);
+  return favoriteArray;
+};
+
+//Front end logic
+$(document).ready(function(){
+  $("#input").submit(function(event) {
+    event.preventDefault();
+
+    var userInput1 = ("#input1").val();
+    var userInput2 = ("#input2").val();
+    var userInput3 = ("#input3").val();
+
+    var results = pushIntoArrays(userInput1, userInput2, userInput3);
+console.log(results);
+    $(".result").text(results.join(" "));
+  });
+});
